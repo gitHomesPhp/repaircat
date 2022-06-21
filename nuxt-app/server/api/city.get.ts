8397647ddc9b33399e32f37e9c994d1c062f5a5e
Evgeniy <peretsmeshnik@gmail.com>
2022-06-21 15:32:22 +0300

    test

--- nuxt-app/server/api/city.get.ts
+++ nuxt-app/server/api/city.get.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig()
-
+    console.log(config.API)
+    console.log(config.API + '/city')
     const data = await $fetch(config.API + '/city')
 
     return data
